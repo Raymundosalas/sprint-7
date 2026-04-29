3ec2f845a661065e69af07a51c90cfe13c916a13
Raymundosalas <128091595+Raymundosalas@users.noreply.github.com>
2026-04-29 06:37:52 -0600

    Create Dashboard.vue

---
+++ Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<h1>Panel Administrativo</h1>
+
+<router-link to="/pacientes">Pacientes</router-link> |
+<router-link to="/citas">Citas</router-link>
+
+<br><br>
+
+<button @click="salir">Cerrar Sesión</button>
+</div>
+</template>
+
+<script setup>
+import {useRouter} from 'vue-router'
+
+const router=useRouter()
+
+const salir=()=>{
+ localStorage.removeItem("token")
+ router.push('/')
+}
+</script>
